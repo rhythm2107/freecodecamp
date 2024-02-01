6bb3f0446490e0f7d523e7bd86cecc30ed92393e
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-02-02 00:31:08 +0100

    Python Automation & SQL DB & JS Visualization Practices

---
+++ gmane/gline.js
@@ -0,0 +1,5 @@
+gline = [ ['Month','umich.edu','indiana.edu','berkeley.edu','ucdavis.edu','uct.ac.za','ufp.pt','columbia.edu','mac.com','gmail.com','virginia.edu'],
+['2005-12',57,12,12,11,14,10,13,12,10,9],
+['2006-01',114,32,33,37,27,32,28,14,23,21],
+['2006-02',24,8,6,1,5,3,3,14,4,6]
+];
